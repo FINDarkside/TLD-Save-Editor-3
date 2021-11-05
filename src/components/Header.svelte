d3b97e63c984d4aa1cae713f69fcf8d72e6602db
Jesse <jesseniininen@gmail.com>
2021-11-05 21:16:31 +0200

    Add material ui library

---
+++ src/components/Header.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { Select } from 'svelte-materialify';
+
+  let items = [
+    { name: 'Save 1', value: '1' },
+    { name: 'Save 2', value: '2' },
+    { name: 'Save 3', value: '3' },
+    { name: 'Save 3', value: '3' },
+    { name: 'Save 3', value: '3' },
+    { name: 'Save 3', value: '3' },
+    { name: 'Save 3', value: '3' },
+    { name: 'Save 3', value: '3' },
+    { name: 'Save 3', value: '3' },
+    { name: 'Save 3', value: '3' },
+    { name: 'Save 3', value: '3' },
+  ];
+</script>
+
+<div class="container">
+  <div class="saveSelection">
+    <Select class="saveSelection" outlined {items}>Outlined</Select>
+  </div>
+</div>
+
+<style>
+  .container {
+    padding: 0.5em;
+  }
+
+  .saveSelection {
+    max-width: 400px;
+  }
+</style>
