625e1f9b450e7cbaa5ac7737b2650ca1677cdc8f
Jesse <jesseniininen@gmail.com>
2022-10-01 18:47:00 +0300

    Add support for parsing -Infinity and NaN

--- src/util/json.ts
+++ src/util/json.ts
@@ -1,15 +1,33 @@
 import JSON5 from 'json5';
 
+// TLD contains invalid JSON which has Infinity, -Infinity and possibly NaN.
+// JSON.parse doesn't support this invalid json so we replace use this token and just replace it with Infinity after serialization
+const infinityToken = 'FsClnI9';
+const negativeInfinityToken = 'LmWSdX0';
+const nanToken = 'EyKTDu6';
+
+const infinityRegex = new RegExp(`"${infinityToken}"`, 'g');
+const negativeInfinityRegex = new RegExp(`"${negativeInfinityToken}"`, 'g');
+const nanRegex = new RegExp(`"${nanToken}"`, 'g');
+
 function parse(json?: string) {
   if (!json) return json;
   return JSON5.parse(json);
   // return JSON.parse(json.replaceAll('Infinity', '0'));
 }
 
-function stringify(json: string) {
-  return JSON.stringify(json, (key, value) => {
-    return value === Infinity ? 'Infinity' : value;
+function stringify(json: any) {
+  const result = JSON.stringify(json, (key, value) => {
+    if (value === Infinity) return infinityToken;
+    if (value === -Infinity) return negativeInfinityToken;
+    if (Number.isNaN(value)) return nanToken;
+    return value === Infinity ? infinityToken : value;
   });
+
+  return result
+    .replaceAll(infinityRegex, 'Infinity')
+    .replaceAll(negativeInfinityRegex, '-Infinity')
+    .replaceAll(nanRegex, 'NaN');
 }
 
 export default {
--- test/parser.spec.ts
+++ test/parser.spec.ts
@@ -11,4 +11,34 @@ describe('Parser', () => {
     const result = parser.parse('{"val": Infinity}');
     expect(result?.val).toEqual(Number.POSITIVE_INFINITY);
   });
+
+  test('Parses -Infinity correctly', () => {
+    const parser = parseObject().json().withFields<ObjWithInfinity>();
+    const result = parser.parse('{"val": -Infinity}');
+    expect(result?.val).toEqual(Number.NEGATIVE_INFINITY);
+  });
+
+  test('Parses NaN correctly', () => {
+    const parser = parseObject().json().withFields<ObjWithInfinity>();
+    const result = parser.parse('{"val": NaN}');
+    expect(Number.isNaN(result?.val)).toBeTruthy();
+  });
+
+  test('Serializes Infinity correctly', () => {
+    const parser = parseObject().json().withFields<ObjWithInfinity>();
+    const json = parser.serialize({ val: Number.POSITIVE_INFINITY });
+    expect(json).toEqual('{"val":Infinity}');
+  });
+
+  test('Serializes -Infinity correctly', () => {
+    const parser = parseObject().json().withFields<ObjWithInfinity>();
+    const json = parser.serialize({ val: Number.NEGATIVE_INFINITY });
+    expect(json).toEqual('{"val":-Infinity}');
+  });
+
+  test('Serializes NaN correctly', () => {
+    const parser = parseObject().json().withFields<ObjWithInfinity>();
+    const json = parser.serialize({ val: Number.NaN });
+    expect(json).toEqual('{"val":NaN}');
+  });
 });
